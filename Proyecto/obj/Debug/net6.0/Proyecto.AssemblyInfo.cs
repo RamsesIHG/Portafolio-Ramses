3cbaf5601fc1965b18b46669f4dfb541141b9770
RamsesIHG <Ramsesherrera56@gmail.com>
2024-05-29 18:21:06 +0200

    Segundo-Avance

--- Proyecto/obj/Debug/net6.0/Proyecto.AssemblyInfo.cs
+++ Proyecto/obj/Debug/net6.0/Proyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7697fe7aa59df20fe6664d86f3a6bc46daf3ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf737e20d1c265b1901d6275233227c8ac0f3518")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
